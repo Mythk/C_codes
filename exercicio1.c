2681797ca7c5f5aa2c0d4b166d9a172d6a8bc259
Mythk <101512061+Mythk@users.noreply.github.com>
2025-03-07 20:58:25 -0300

    Vetores_1

---
+++ exercicio1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    int vetor [4];
+    vetor[0] = 10;
+    vetor[1] = 46;
+    vetor[2] = 41;
+    vetor[3] = 60;
+    vetor[4] = 200;
+
+    printf("Valores do vetor:\n");
+    for (int i = 0; i < 6; i++)
+    {
+        printf("vetor[%d] = %d\n", i, vetor[i]);
+    }
+    return 0;
+}
