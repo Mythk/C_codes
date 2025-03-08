59e1894b042972c4050c368fb573e6a6d61a79bc
Mythk <101512061+Mythk@users.noreply.github.com>
2025-03-07 21:29:57 -0300

    Vetores_4
    Programa que preenche um vetor de 20
    posições e exibe todos os valores pares e suas
    respectivas posições.

---
+++ exercicio4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <locale.h>
+
+#define 20
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    int numeros[20], i;
+
+    for (i = 0; i < TAM; i++) {
+        printf("Posição %d do vetor: ", i);
+        scanf("%d", &numeros[i]);
+    }
+        printf("\n\n");
+
+        for(i = 0; i < TAM; i++) {
+            if(numeros[i] % 2 == 0) {
+                printf("\nElemento %d na posição %d\n", numeros[i]);
+
+            }
+        }
+            return 0;
+}
